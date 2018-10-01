a2d2863ffbd2e9f834ae0b9aa7af60fd21d14096
Vadim Zavertaluik <zavertaluik.v@gmail.com>
2018-10-01 08:37:52 +0300

    Fixing fourth task.

--- zavertalyuk_vadim/lesson2/fourthTask.js
+++ zavertalyuk_vadim/lesson2/fourthTask.js
@@ -1,10 +1,14 @@
 function countFibonacci(number) {
-    if (number === 1) {
-        return 1;
+    var a = 1, b = 0, temp;
+    var mass = [];
+    while (number >= 1) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        mass.push(b);
+        number--;
     }
-    else {
-        return number * countFibonacci(number - 1);
-    }
+    return mass;
 }
 
 console.log("Fourth task with number of elements is :" + 5);
